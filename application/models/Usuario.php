c3c30389b516bcad3ae5bccd3c9ef30f35fa0ce2
Guilherme Henrique <progr.guilherme@gmail.com>
2017-07-20 17:12:42 -0300

    user model

---
+++ application/models/Usuario.php
@@ -0,0 +1,29 @@
+<?php
+class Usuario extends CI_Model {
+
+        public function __construct()
+        {
+                parent::__construct();
+                $this->load->database();        
+        }
+        public function verifyFbUser($id)
+        {
+                $this->db->select('id');
+                $this->db->from('usuarios_facebook');
+                $this->db->where('fb_id', $id);
+                $this->db->limit(1);
+                $query = $this->db->get();
+                return $query->row('id');
+        }
+        public function insertFbUser($id, $name)
+        {
+                $data = array(
+                        'fb_id' => $id,
+                        'fb_name' => $name
+                );
+
+                $query = $this->db->insert('usuarios_facebook', $data);
+                return $this->db->insert_id();                
+        }        
+
+}
