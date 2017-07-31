22df1dc4993536136fea0c600012f1a2626c6176
Guilherme Henrique <progr.guilherme@gmail.com>
2017-07-31 11:01:14 -0300

    change menu links

--- application/views/template/menu.php
+++ application/views/template/menu.php
@@ -3,8 +3,8 @@ MENU DA PAGINA
 -->
 <nav>
 	<ul id="slide-out" class="side-nav">
-		<li><a class="waves-effect grey darken-2" href="#!" >Empregos</a></li>
-		<li><a class="waves-effect" href="#!">Candidatos</a></li>
+		<li><a class="waves-effect grey darken-2" href="<?=base_url('empregos')?>" >Empregos</a></li>
+		<li><a class="waves-effect" href="<?=base_url('candidatos')?>">Candidatos</a></li>
 		<li><div class="divider"></div></li>
 		<li><a class="waves-effect" id="servico" 	href="#LoginModal">Anunciar Serviço</a></li>
 		<li><a class="waves-effect" id="vaga" 		href="#LoginModal">Anunciar Vaga</a></li>
@@ -15,8 +15,8 @@ MENU DA PAGINA
 	<a href="#" data-activates="slide-out" class="button-collapse"><i class="material-icons">menu</i></a>	
 	<div class="nav-wrapper grey darken-4">
 		<ul class="left hide-on-med-and-down">
-			<li class="grey darken-2"><a href="#">Empregos</a></li>
-			<li><a href="#">Candidatos</a></li>
+			<li class="grey darken-2"><a href="<?=base_url('empregos')?>">Empregos</a></li>
+			<li><a href="<?=base_url('candidatos')?>">Candidatos</a></li>
 			<li><a href="#SearchModal" class="waves-effect waves-light btn cyan darken-3">Busca</a></li>
 		</ul>
 		<a href="<?=base_url()?>" class="brand-logo center">
