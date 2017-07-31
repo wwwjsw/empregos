f26094bcd911f106d7056ff2289ffed100dfb2f8
Guilherme Henrique <progr.guilherme@gmail.com>
2017-07-31 15:06:17 -0300

    adds base_url()

--- application/views/template/footer.php
+++ application/views/template/footer.php
@@ -28,7 +28,7 @@
 </footer>
 <!--Import jQuery before materialize.js-->
 <script type="text/javascript" src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
-<script type="text/javascript" src="materialize/js/materialize.min.js"></script>
+<script type="text/javascript" src="<?=base_url('materialize/js/materialize.min.js')?>"></script>
 <script>
 	$(document).ready(function(){
     // the "href" attribute of the modal trigger must specify the modal ID that wants to be triggered
