5488169c018c9ce20a91205e38aedf619f3eb525
Guilherme Henrique <progr.guilherme@gmail.com>
2017-08-03 11:49:56 -0300

    nonbreak base_url()

--- application/views/administrativo/header.php
+++ application/views/administrativo/header.php
@@ -6,9 +6,9 @@
 	<!--Import Google Icon Font-->
 	<link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
 	<!--Import materialize.css-->
-	<link type="text/css" rel="stylesheet" href="materialize/css/materialize.min.css"  media="screen,projection"/>
+	<link type="text/css" rel="stylesheet" href="<?=base_url('materialize/css/materialize.min.css')?>"  media="screen,projection"/>
 	<!--Import home page Css-->
-	<link rel="stylesheet" href="assets/css/home.css"/>
+	<link rel="stylesheet" href="<?=base_url('assets/css/home.css')?>"/>
 
 	<!--Let browser know website is optimized for mobile-->
 	<meta name="viewport" content="width=device-width, initial-scale=1.0"/>	
