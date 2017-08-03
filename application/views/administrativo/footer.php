0772113d1df15a09a03f0b249f30f3ea26dbd4dd
Guilherme Henrique <progr.guilherme@gmail.com>
2017-08-03 11:52:14 -0300

    nonbreak base_url(), js

--- application/views/administrativo/footer.php
+++ application/views/administrativo/footer.php
@@ -1,6 +1,6 @@
 <!--Import jQuery before materialize.js-->
 <script type="text/javascript" src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
-<script type="text/javascript" src="materialize/js/materialize.min.js"></script>
+<script type="text/javascript" src="<?=base_url('materialize/js/materialize.min.js')?>"></script>
 <script>
 (function() {
 	$('.dropdown-button').dropdown({
