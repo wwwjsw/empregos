49e8786aaf0a5f6d602796b1f293ee19fddd8030
Guilherme Henrique <progr.guilherme@gmail.com>
2017-08-07 09:36:30 -0300

    change footer color class, insert logo

--- application/views/template/footer.php
+++ application/views/template/footer.php
@@ -1,21 +1,11 @@
 <!--
 	COMEÇO DO FOOTER
 -->
-<footer class="page-footer grey darken-1">
+<footer class="page-footer blue-grey darken-1">
 	<div class="container">
 		<div class="row">
-			<div class="col l6 s12">
-				<h5 class="white-text">Footer</h5>
-				<p class="grey-text text-lighten-4">conteudo do footer</p>
-			</div>
-			<div class="col l4 offset-l2 s12">
-				<h5 class="white-text">Links</h5>
-				<ul>
-					<li><a class="grey-text text-lighten-3" href="#!">Link 1</a></li>
-					<li><a class="grey-text text-lighten-3" href="#!">Link 2</a></li>
-					<li><a class="grey-text text-lighten-3" href="#!">Link 3</a></li>
-					<li><a class="grey-text text-lighten-3" href="#!">Link 4</a></li>
-				</ul>
+			<div class="col l12 s12">
+                <img style="height: 50px;" class="responsive-img" src="<?=base_url('assets/images/logo.png')?>" alt="">
 			</div>
 		</div>
 	</div>
