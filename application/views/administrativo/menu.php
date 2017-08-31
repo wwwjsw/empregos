63403a9b54061596e4c34cbfb48b94418c9f9d9e
guilherme <progr.guilherme@gmail.com>
2017-08-31 13:31:26 -0300

    administrative home menu link

--- application/views/administrativo/menu.php
+++ application/views/administrativo/menu.php
@@ -5,7 +5,7 @@
 		<div class="nav-wrapper grey darken-4">
 			<img src="<?=base_url('assets/images/logo.png')?>" class="responsive-img" style="height: 50px; margin-top: 5px;">
 			<ul id="nav-mobile" class="right hide-on-med-and-down">
-                <li><a href="#">Publicações</a></li>
+                <li><a href="<?=base_url('administrativo')?>">Publicações</a></li>
                 <li><a href="<?=base_url('administrativo/empregos')?>">Empregos</a></li>
 				<li><a href="#">Pessoas</a></li>
 				<li><a href="#">Configurações</a></li>
