f362154ce67c1bcc8edc14f3a97d3e8fc24297f4
guilherme <progr.guilherme@gmail.com>
2017-08-31 16:04:09 -0300

    include users, remove config

--- application/views/administrativo/menu.php
+++ application/views/administrativo/menu.php
@@ -7,8 +7,7 @@
 			<ul id="nav-mobile" class="right hide-on-med-and-down">
                 <li><a href="<?=base_url('administrativo')?>">Publicações</a></li>
                 <li><a href="<?=base_url('administrativo/empregos')?>">Empregos</a></li>
-				<li><a href="#">Pessoas</a></li>
-				<li><a href="#">Configurações</a></li>
+				<li><a href="<?=base_url('administrativo/pessoas')?>">Pessoas</a></li>
 				<li><a href="<?=base_url('administrativo/logout')?>">Sair</a></li>
 			</ul>
 		</div>
