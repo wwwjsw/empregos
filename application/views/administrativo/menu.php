51c9581ea4d1392cc8c41ee025848712f16a1b05
guilherme <progr.guilherme@gmail.com>
2017-08-31 15:18:52 -0300

    logo link

--- application/views/administrativo/menu.php
+++ application/views/administrativo/menu.php
@@ -3,7 +3,7 @@
 -->
 	<nav>
 		<div class="nav-wrapper grey darken-4">
-			<img src="<?=base_url('assets/images/logo.png')?>" class="responsive-img" style="height: 50px; margin-top: 5px;">
+            <a href="<?=base_url('administrativo')?>"><img src="<?=base_url('assets/images/logo.png')?>" class="responsive-img" style="height: 50px; margin-top: 5px;"></a>
 			<ul id="nav-mobile" class="right hide-on-med-and-down">
                 <li><a href="<?=base_url('administrativo')?>">Publicações</a></li>
                 <li><a href="<?=base_url('administrativo/empregos')?>">Empregos</a></li>
